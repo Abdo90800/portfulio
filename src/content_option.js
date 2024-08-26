f37c5f453f7d4649f39900494c9749aae709b0f9
Abdo90800 <islammsalah8@gmail.com>
2024-08-26 12:35:11 +0300

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,111 @@
+const logotext = "Eng Abdo";
+const meta = {
+    title: "Eng Abdo",
+    description: "I’m Eng Abdo data scientist _ Frontend devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Abdo mostafa",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+    jobtitle: "Designer of week",
+    where: "alexandia",
+    date: "2020",
+}, ];
+
+const skills = [{
+        name: "Javascript",
+        value: 50,
+    },
+    {
+        name: "HTML",
+        value: 80,
+    },
+    {
+        name: "css",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "tailwend",
+        value: 60,
+    },
+    {
+        name: "mui",
+        value: 70,
+    },
+];
+
+const services = [{
+    title: "frontend developer",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+}, ];
+
+const dataportfolio = [{
+        id: 1,
+        image: "./images/dashpord.png",
+        description: "first ",
+        link: "",
+    },
+    // {
+    //     id: 2,
+    //     image: "./images/dashpord.png",
+    //     description: "first project dashpord",
+    //     link: "https://dashbord-dev.netlify.app/",
+    // },
+    // {
+    //     id: 3,
+    //     image: "./images/dashpord.png",
+    //     description: "first project dashpord",
+    //     link: "https://dashbord-dev.netlify.app/",
+    // },
+    // {
+    //     id: 4,
+    //     image: "./images/dashpord.png",
+    //     description: "first project dashpord",
+    //     link: "https://dashbord-dev.netlify.app/",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Engabdo.dev@gmail.com",
+    YOUR_FONE: "01501588795",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/portfolio/index.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import { dataportfolio, meta } from "../../content_option";
+import images from "../../images/dashpord.png";
+import images1 from "../../images/0.png";
+import images2 from "../../images/3.png";
+import images3 from "../../images/4.png";
+import images4 from "../../images/eco.png";
+import images5 from "../../images/ecomstore.png";
+
+export const Portfolio = () => {
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> Portfolio | {meta.title} </title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>{" "}
+        </Row>{" "}
+        <div className="mb-5 po_items_ho">
+          {" "}
+          {dataportfolio.map((data, i) => {
+            console.log(data);
+            return (
+              <div key={i}>
+                <div className="po_item">
+                  <img src={images} alt="" />
+                  <div className="content">
+                    <p> {data.description} </p>{" "}
+                    <a href="https://dashbord-dev.netlify.app"> view project </a>
+                  </div>
+                </div>
+                <div className="po_item">
+                  <img src={images5} alt="" />
+                  <div className="content">
+                    <p> {data.description} </p>{" "}
+                    <a href="https://guileless-zabaione-2f9115.netlify.app">
+                      {" "}
+                      view project{" "}
+                    </a>
+                  </div>
+                </div>
+                <div className="po_item">
+                  <img src={images1} alt="" />
+                  <div className="content">
+                    <p> {data.description} </p>{" "}
+                    <a href="https://remarkable-raindrop-fac6bf.netlify.app">
+                      {" "}
+                      view project{" "}
+                    </a>
+                  </div>
+                </div>{" "}
+                <div className="po_item">
+                  <img src={images3} alt="" />
+                  <div className="content">
+                    <p> {data.description} </p>{" "}
+                    <a href="https://vermillion-puffpuff-1b0b1b.netlify.app">
+                      {" "}
+                      view project{" "}
+                    </a>
+                  </div>
+                </div>
+                <div className="po_item">
+                  <img src={images2} alt="" />
+                  <div className="content">
+                    <p> {data.description} </p>{" "}
+                    <a href="https://fabulous-yeot-6d00af.netlify.app">
+                      {" "}
+                      view project{" "}
+                    </a>
+                  </div>
+                </div>
+                <div className="po_item">
+                  <img src={images4} alt="" />
+                  <div className="content">
+                    <p> {data.description} </p>{" "}
+                    <a href="https://candid-moxie-bbb2b1.netlify.app">
+                      {" "}
+                      view project{" "}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Container>
+    </HelmetProvider>
+  );
+};
